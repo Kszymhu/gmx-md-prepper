04245f9e496557fdb92441926cbe52dcd03898b8
Krzysztof Czołczyński <43497626+Kszymhu@users.noreply.github.com>
2023-03-18 16:47:10 +0100

    Update cli.py

--- src/gmx_md_prepper/cli.py
+++ src/gmx_md_prepper/cli.py
@@ -2,7 +2,7 @@ from argparse import ArgumentParser
 
 def cli_test() -> None:
     parser = ArgumentParser()
-    parser.add_argument('-m', '--message', action="store_true")
+    parser.add_argument('-m', '--message', type=str)
     
     args = parser.parse_args()
     print(args.message)
