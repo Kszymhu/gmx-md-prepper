a681062bec9067129bce12ee7757beb1ada3ea27
Krzysztof Czołczyński <43497626+Kszymhu@users.noreply.github.com>
2023-03-28 17:11:38 +0200

    Update cli.py

--- src/gmx_md_prepper/cli.py
+++ src/gmx_md_prepper/cli.py
@@ -1,8 +1,23 @@
 from argparse import ArgumentParser
 
-def cli_test() -> None:
+def cli_main() -> None:
     parser = ArgumentParser()
-    parser.add_argument('-m', '--message', type=str)
-    
+    parser.add_argument(
+        '-s',
+        '--structure',
+        type=str,
+        required=True,
+        help='File containing protein structure, in a format accepted by GROMACS\'s pdb2gmx.'
+    )
+
+    parser.add_argument(
+        '-ff',
+        '--forcefield',
+        type=str,
+        required=True,
+        help='Directory containing your forcefield files.'
+    )
+
     args = parser.parse_args()
-    print(args.message)
+    print(f'Structure file: {args.structure}')
+    print(f'Forcefield: {args.forcefield}')
